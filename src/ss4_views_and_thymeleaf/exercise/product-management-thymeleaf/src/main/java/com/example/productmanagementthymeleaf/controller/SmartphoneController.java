ebbee460bf14a97273978375a0e553d17137a833
hau8477 <hau8477@gmail.com>
2023-02-04 10:30:13 +0700

    ss3 data biding & form

--- src/ss4_views_and_thymeleaf/exercise/product-management-thymeleaf/src/main/java/com/example/productmanagementthymeleaf/controller/SmartphoneController.java
+++ src/ss4_views_and_thymeleaf/exercise/product-management-thymeleaf/src/main/java/com/example/productmanagementthymeleaf/controller/SmartphoneController.java
@@ -5,8 +5,8 @@ import com.example.productmanagementthymeleaf.service.ISmartphoneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/smartphone")
@@ -15,14 +15,42 @@ public class SmartphoneController {
     private ISmartphoneService smartphoneService;
 
     @GetMapping("")
-    public String home(Model model){
-        model.addAttribute("smartphones",smartphoneService.findAll());
+    public String home(Model model) {
+        model.addAttribute("smartphones", smartphoneService.findAll());
+        model.addAttribute("smartphone", new SmartPhone());
         return "/index";
     }
 
-    @GetMapping("")
-    public String layout(Model model){
-        model.addAttribute("smartphone", new SmartPhone());
-        return "/layout";
+    @PostMapping("/create")
+    public String create(@ModelAttribute("smartphone") SmartPhone smartPhone, RedirectAttributes attributes) {
+        smartPhone.setId((int) (Math.random() * 10000));
+        smartphoneService.save(smartPhone);
+        return "redirect: /smartphone";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("smartphone", smartphoneService.findById(id));
+        return "/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Model model, @ModelAttribute("smartphone") SmartPhone smartPhone) {
+        smartphoneService.update(smartPhone);
+        model.addAttribute("message", "Update successful!");
+        return "/edit";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("idDelete") int id, RedirectAttributes attributes) {
+        smartphoneService.remove(id);
+        attributes.addFlashAttribute("mess", "Delete successful!");
+        return "redirect: /smartphone";
+    }
+
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable("id") int id, Model model) {
+        model.addAttribute("smartphone", smartphoneService.findById(id));
+        return "/view";
     }
 }
