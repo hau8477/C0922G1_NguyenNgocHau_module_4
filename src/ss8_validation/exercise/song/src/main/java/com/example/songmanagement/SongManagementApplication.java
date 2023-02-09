b1043ff4284c82fa28843da5af4379521a9db084
hau8477 <hau8477@gmail.com>
2023-02-09 20:40:38 +0700

    ss7 spring data repository

---
+++ src/ss8_validation/exercise/song/src/main/java/com/example/songmanagement/SongManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.songmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongManagementApplication.class, args);
+    }
+
+}
